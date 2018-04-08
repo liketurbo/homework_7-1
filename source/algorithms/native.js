f69a7a2ecad679f67295883861df65abd6554755
Ramzan Chasygov <theliketurbo@gmail.com>
2018-04-09 00:20:53 +0300

    new demo

--- source/algorithms/native.js
+++
@@ -1,7 +0,0 @@
-export default (list: string[], input: string): string[] => {
-  if (input === '') return list;
-
-  return list.filter(elem => (
-    elem.toLowerCase().includes(input.toLowerCase())
-  ));
-};
--- source/components/speed.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default ({ speed }) => (
-  <div className="speed">
-    <span className="speed__start">Speed&nbsp;</span>
-    <span className="speed__content">{Math.round(speed * 100) / 100}</span>
-    <span className="speed__end">&nbsp;milliseconds</span>
-  </div>
-);
